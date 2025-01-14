c89787c89d65f49f37d5ef21d8fa7aa8b8ffce66
ruhangaratini <garatini.ruhan@gmail.com>
2025-01-14 14:08:01 -0300

    First commit

---
+++ src/main/java/com/example/POO_Biblioteca/PooBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.POO_Biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/POO_Biblioteca/PooBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.POO_Biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
